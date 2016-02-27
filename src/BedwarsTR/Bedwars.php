270e037e1b546ae4d02ffed76cf59930eec7787f
Emre Yavuzyiğit <emre.yavuz.842@gmail.com>
2016-02-27 20:37:06 +0200

    ÇOK DAHA FAZLA KOD EKLENDİ!
    Alo ?

--- src/BedwarsTR/Bedwars.php
+++ src/BedwarsTR/Bedwars.php
@@ -1,6 +1,9 @@
 
 <?php
 
+  /////////////////////////////////////
+ ///// I N  D E V E L O P M E N T/////
+/////////////////////////////////////
 namespace BedwarsTR;
 
 use pocketmine\plugin\PluginBase;
@@ -19,6 +22,7 @@ use pocketmine\player;
 use pocketmine\level\Level;
 use pocketmine\level\Position;
 use pocketmine\math\Vector3;
+use pocketmine\command\ConsoleCommandSender;
 use pocketmine\level\sound\ClickSound;
 use pocketmine\level\sound\PopSound;
 use pocketmine\level\sound\FizzSound;
@@ -35,16 +39,59 @@ private $steam;
 private $yteam;
 
     public function onEnable(){
-      $this->getLogger()->info("§8[§cBed§4WarsTR§8]§a Plugin aktifleştirildi")
+      $this->getLogger()->info("§8[§cBed§4WarsTR§8]§a Plugin aktifleştirildi");
+      $this->getScheduler()->getschedulerRepeatingTask(new CallbackTask([$this,"lobbysan"]),20);
+      $this->saniye=0;
     }
     public function OnDisable(){
-        $this->getLogger()->inf("§8[§cBed§4Wars§8]§c Plugin Deaktifleştirildi")
+        $this->getLogger()->info("§8[§cBed§4Wars§8]§c Plugin Deaktifleştirildi");
+        $this->getScheduler()->getschedulerRepeatingTask(new CallbackTask([$this,"lobbysan"]),20);
     }
     public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
         switch($cmd->getName()){
-            case "bw";
+            case "bw":
+            $sender->sendMessage("§6Tüm Komutlar:");
+            $sender->sendMessage("§d - /bw başlat :§e Oyunu Başatır(Adminler İçin)");
+$sender->sendMessage("§b- /bw katıl :§e BedWarsa Katılınılır.");
+$sender->sendMessage("§6©===========================©");
+            break;
+            case "başlat":
+            $this->saniye=10;
+            $this->lobbysan;
+            case "katıl":
+            $this->lobbysan;
+            $this->saniye=60;
+            break;
         }
     }
+public function lobbysan(){
+$this->saniye--;
+switch($this->saniye){
+case 1:
+case 2:
+case 3:
+case 4:
+case 5:
+case 6:
+case 7:
+case 8:
+case 9:
+case 10:
+    $this->sendMessage(TextFormat::GREEN. " <BedWars> Başlamasına " .$this->saniye. " saniye..");
+    //$p->getLevel()->addSound(new ClickSound($p)); Yakinda!
+    //$p->getLevel()->addParticle(new DustParticle(new Position($p))); Yakinda!
+break;
+case 30:
+    $this->sendMessage(TextFormat::GREEN. "<BedWars> Başlamasına 30 saniye");
+break;
+case 60:
+    $this->sendMessage(TextFormat::GREEN. "<BedWars> Baslamasına 1 Dakika.");
+break;
+case 0:
+    $this->sendMessage(TextFormat::GOLD. " OYUN BAŞLADI");
+    //$p->teleport(new Vector3(0,0,0)); Yakinda!
+}
+}
 
 
 }
