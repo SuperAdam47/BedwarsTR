d3e5742d140e02a9134a26f513ed78c4bfdda9ec
Enes Yıldırım <enes5519@gmail.com>
2016-02-27 19:23:32 +0200

    Update Bedwars.php

--- src/BedwarsTR/Bedwars.php
+++ src/BedwarsTR/Bedwars.php
@@ -10,10 +10,15 @@ use pocketmine\command\CommandSender;
 
 class Bedwars extends PluginBase implements Listener{
     public function onEnable(){
-      $this->getLogger()->info("§6[§fBedWarsTR§6]§a Plugin aktifleştirildi")
+      $this->getLogger()->info("§8[§cBed§4WarsTR§8]§a Plugin aktifleştirildi")
+    }
+    public funcrion odisable(){
+        $this->getLogger()->inf("§8[§cBed§4Wars§8]§c Plugin Deaktifleştirildi")
     }
     public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
-
+        switch($cmd->getName()){
+            case "bw";
+        }
     }
 
 }
